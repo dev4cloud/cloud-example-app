2c46e42aba194754450e050a4445dc63163af7fb
Joseph Gette <joseph.gette@ibm.com>
2021-03-30 11:04:13 +0200

    Initial project structure

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    java
+    id("org.springframework.boot") version "2.4.4"
+}
+
+apply(plugin = "io.spring.dependency-management")
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+extra["junitVersion"] = "5.4.2"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${project.extra["junitVersion"]}")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:${project.extra["junitVersion"]}")
+    testImplementation("org.junit.jupiter:junit-jupiter:${project.extra["junitVersion"]}")
+}
