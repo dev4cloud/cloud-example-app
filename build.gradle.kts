0d920faa324b69a9046a9584d14b93ac9e2314e1
Joseph Gette <joseph.gette@ibm.com>
2021-04-15 15:58:01 +0200

    Use IBM Cloud DB

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ extra["junitVersion"] = "5.4.2"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    runtimeOnly("com.h2database:h2")
+    implementation("org.postgresql:postgresql")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${project.extra["junitVersion"]}")
     testImplementation("org.junit.jupiter:junit-jupiter-params:${project.extra["junitVersion"]}")
     testImplementation("org.junit.jupiter:junit-jupiter:${project.extra["junitVersion"]}")
--- src/main/java/com/cloud/example/DataSourceConfig.java
+++ src/main/java/com/cloud/example/DataSourceConfig.java
@@ -9,26 +9,25 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
 
-//    @Bean
-//    public DataSource getDataSource() {
-//        String dbName = System.getenv("APP_DATABASE");
-//        String dbUsername = System.getenv("APP_DATABASE_USER");
-//        String dbPassword = System.getenv("APP_DATABASE_PASSWORD");
-//        System.out.println("Using DATABASE: " + dbName);
-//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-//        dataSourceBuilder.url("jdbc:mysql://hdmdemo.cluster-cwaf6q1jfqoy.eu-west-1.rds.amazonaws.com:3306/" + dbName);
-//        dataSourceBuilder.username(dbUsername);
-//        dataSourceBuilder.password(dbPassword);
-//        return dataSourceBuilder.build();
-//    }
-
     @Bean
     public DataSource getDataSource() {
+        String dbName = System.getenv("APP_DATABASE");
+        String dbUsername = System.getenv("APP_DATABASE_USER");
+        String dbPassword = System.getenv("APP_DATABASE_PASSWORD");
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.h2.Driver");
-        dataSourceBuilder.url("jdbc:h2:mem:test");
-        dataSourceBuilder.username("SA");
-        dataSourceBuilder.password("");
+        dataSourceBuilder.url("jdbc:postgresql://dbaas903.hyperp-dbaas.cloud.ibm.com:29050/" + dbName);
+        dataSourceBuilder.username(dbUsername);
+        dataSourceBuilder.password(dbPassword);
         return dataSourceBuilder.build();
     }
+
+//    @Bean
+//    public DataSource getDataSource() {
+//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+//        dataSourceBuilder.driverClassName("org.h2.Driver");
+//        dataSourceBuilder.url("jdbc:h2:mem:test");
+//        dataSourceBuilder.username("SA");
+//        dataSourceBuilder.password("");
+//        return dataSourceBuilder.build();
+//    }
 }
