d5ad3fa2329c7b1524c2aab3a23f435ea20c703b
Joseph Gette <joseph.gette@ibm.com>
2021-05-26 17:42:31 +0200

    Add Dockerfile

--- src/main/java/com/cloud/example/DataSourceConfig.java
+++ src/main/java/com/cloud/example/DataSourceConfig.java
@@ -21,14 +21,4 @@ public class DataSourceConfig {
         dataSourceBuilder.password(dbPassword);
         return dataSourceBuilder.build();
     }
-
-//    @Bean
-//    public DataSource getDataSource() {
-//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-//        dataSourceBuilder.driverClassName("org.h2.Driver");
-//        dataSourceBuilder.url("jdbc:h2:mem:test");
-//        dataSourceBuilder.username("SA");
-//        dataSourceBuilder.password("");
-//        return dataSourceBuilder.build();
-//    }
 }
